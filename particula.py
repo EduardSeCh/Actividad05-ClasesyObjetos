b05a672c6fda89de4c66c96e254cc4b0f601ac45
eduard <eduardogsech@gmail.com>
2022-10-20 15:07:18 -0500

    Proyecto Terminado

--- particula.py
+++ particula.py
@@ -1,4 +1,5 @@
-from algoritmos import distancia_euclidiana
+from .algoritmos import distancia_euclidiana
+#.algoritmos para que la carpeta principal la tome
 class Particula():
     def __init__(self,id=0,origen_x=0,origen_y=0,destino_x=0,destino_y=0,velocidad=0,red=0,green=0,blue=0):
         self.__id = id
@@ -14,14 +15,14 @@ class Particula():
         
     def __str__(self) -> str:
         return(
-            "Id: \t\t"+ str(self.__id)+'\n'+
+            "Id: \t"+ str(self.__id)+'\n'+
             "origen_x: \t"+ str(self.__origen_x)+'\n'+
             "origen_y: \t"+ str(self.__origen_y)+'\n'+
             "destino_x: \t"+ str(self.__destino_x)+'\n'+
             "destino_y: \t"+ str(self.__destino_y)+'\n'+
             "velocidad: \t"+ str(self.__velocidad)+'\n'+
-            "red: \t\t"+ str(self.__red)+'\n'+
-            "green: \t\t"+ str(self.__green)+'\n'+
-            "blue: \t\t"+ str(self.__blue)+'\n'+
+            "red: \t"+ str(self.__red)+'\n'+
+            "green: \t"+ str(self.__green)+'\n'+
+            "blue: \t"+ str(self.__blue)+'\n'+
             "distancia: \t"+ str(self.__distancia)+'\n')
         
--- particulas.py
+++ particulas.py
@@ -1,5 +1,5 @@
-from particula import Particula
-
+from .particula import Particula
+#.partiucla para que la carpeta principal la tome
 class Particulas:
     def __init__(self):
         self.__particulas = []
@@ -14,11 +14,7 @@ class Particulas:
         for particula in self.__particulas:
             print(particula)
             
-preuba = Particula(1,2,3,4,5,6,7,8,9)
-prueba2 = Particula(10,20,30,40,50,60,70,80,90)
-prueba3 = Particula(65,7,4,8,3,9,1,78,70)
-particulas = Particulas()
-particulas.agregar_inicio(preuba)
-particulas.agregar_final(prueba3)
-particulas.agregar_inicio(prueba2)
-particulas.mostrar()
+    def __str__(self) -> str:
+        return "".join(
+            str(particula) + "\n" for particula in self.__particulas
+        )
